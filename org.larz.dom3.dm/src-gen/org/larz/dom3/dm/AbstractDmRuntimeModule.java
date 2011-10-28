ff01d55d4331a1fc974371adece994ecbb6395af
larzsp <larzsp@gmail.com>
2011-10-28 23:42:43 +0000

    Latest generated source.

--- org.larz.dom3.dm/src-gen/org/larz/dom3/dm/AbstractDmRuntimeModule.java
+++ org.larz.dom3.dm/src-gen/org/larz/dom3/dm/AbstractDmRuntimeModule.java
@@ -45,7 +45,7 @@ public abstract class AbstractDmRuntimeModule extends DefaultRuntimeModule {
 	}
 
 	// contributed by org.eclipse.xtext.generator.parser.antlr.XtextAntlrGeneratorFragment
-	public Class<? extends org.eclipse.xtext.parser.antlr.IAntlrParser> bindIAntlrParser() {
+	public Class<? extends org.eclipse.xtext.parser.IParser> bindIParser() {
 		return org.larz.dom3.dm.parser.antlr.DmParser.class;
 	}
 
