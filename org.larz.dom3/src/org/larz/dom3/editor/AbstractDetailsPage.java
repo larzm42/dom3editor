8d121f2bbdc9f7fc2cde97dac6870a337004cc80
larzsp <larzsp@01bcc9fa-52ed-0a9e-3258-f04e8f663860>
2011-11-04 18:06:07 +0000

    Log message fix.

--- org.larz.dom3/src/org/larz/dom3/editor/AbstractDetailsPage.java
+++ org.larz.dom3/src/org/larz/dom3/editor/AbstractDetailsPage.java
@@ -52,7 +52,7 @@ import org.larz.dom3.image.ImageConverter;
 import org.larz.dom3.image.ImageLoader;
 
 public abstract class AbstractDetailsPage implements IDetailsPage {
-	public static final int DEFAULT_VALUE_WIDTH = 50;
+	public static final int DEFAULT_VALUE_WIDTH = 60;
 	
 	protected static Map<String, Image> spriteMap = new HashMap<String, Image>();
 
