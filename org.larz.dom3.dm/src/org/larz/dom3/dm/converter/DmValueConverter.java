ba4533a6253a28aceff4232ebd591b0e133cd756
larzsp <larzsp@01bcc9fa-52ed-0a9e-3258-f04e8f663860>
2012-02-01 18:07:52 +0000

    Don't convert string into java escaped string.

--- org.larz.dom3.dm/src/org/larz/dom3/dm/converter/DmValueConverter.java
+++ org.larz.dom3.dm/src/org/larz/dom3/dm/converter/DmValueConverter.java
@@ -19,10 +19,23 @@ import org.eclipse.xtext.common.services.DefaultTerminalConverters;
 import org.eclipse.xtext.conversion.IValueConverter;
 import org.eclipse.xtext.conversion.ValueConverterException;
 import org.eclipse.xtext.conversion.impl.AbstractValueConverter;
+import org.eclipse.xtext.conversion.impl.STRINGValueConverter;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.util.Strings;
 
 public class DmValueConverter extends DefaultTerminalConverters {
+
+	@Override
+	public IValueConverter<String> STRING() {
+		return new STRINGValueConverter() {
+
+			@Override
+			protected String toEscapedString(String value) {
+				return '"' + value.toString() + '"';
+			}
+
+		};
+	}
 
 	@Override
 	public IValueConverter<Integer> INT() {
